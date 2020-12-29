f6d5a381e635aacff4350737bbe30ce45423f2e6
Voroin Vyacheslav <vvoronin@list.ru>
2020-12-29 08:11:41 +0300

    Commit

--- src/Lesson_4/Task_3.java
+++ src/Lesson_4/Task_3.java
@@ -8,5 +8,19 @@ public class Task_3 {
         Написать программу для вывода на экран таблицы умножения.
 
      */
+
+        int x=1, y=1;
+
+        System.out.println("Программа выводит таблицу умножения: ");
+        System.out.println("=====================================================");
+        System.out.println();
+
+        while (y<=10){
+            System.out.println("Таблица умножения для: " + y);
+            for(x=1; x<=10; x++)
+                System.out.println(" " + y +" * " + x + " = " + x*y);
+            System.out.println();
+            y++;
+        }
     }
 }
